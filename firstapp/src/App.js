4c8c97a1d57c5d5efe584c86591f39ce1d2af191
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-03-27 00:38:12 +0200

    implemented InputComponent

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,11 +1,12 @@
 import Btn from "./Btn.js"
 import ModeToggler from "./ModeToggler"
+import InputComponent from "./InputComponent.js";
 
 const Nav = (props) => <ul><li>{props.first}</li></ul>
 
 function App(props) {
 	return (
-		<ModeToggler />
+		<InputComponent />
 	);
 }
 
