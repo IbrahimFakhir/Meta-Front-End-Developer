acdbbb74b425d89dcba8a79beb22ac4b5551e08f
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-03-26 19:11:57 +0200

    cleared working space and used arrow function

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,35 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function Heading() {
-
-	return <h1>This is a h1 heading</h1>
-
+const Nav = (props) => {
+	return (
+		<ul>
+			<li>{props.first}</li>
+		</ul>
+	)
 }
 
-function App() {
-
-	return <Heading/>
-
-	/*return (
-		<div className="App">
-		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-			<p>
-			Edit <code>src/App.js</code> and save to reload.
-			</p>
-			<a
-			className="App-link"
-			href="https://reactjs.org"
-			target="_blank"
-			rel="noopener noreferrer"
-			>
-			Learn React
-			</a>
-		</header>
-		</div>
-	);*/
-
+function App(props) {
+	return <Nav first="yo" />
 }
 
 export default App;
