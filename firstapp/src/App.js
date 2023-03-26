d5b6b017c59250094a04b1afa82d5aa9e5e194e0
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-03-26 22:59:04 +0200

    implemented ModeToggler

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,10 +1,11 @@
 import Btn from "./Btn.js"
+import ModeToggler from "./ModeToggler"
 
 const Nav = (props) => <ul><li>{props.first}</li></ul>
 
 function App(props) {
 	return (
-		<Btn />
+		<ModeToggler />
 	);
 }
 
--- firstapp/src/ModeToggler.js
+++ firstapp/src/ModeToggler.js
@@ -1,3 +1,19 @@
 export default function ModeToggler() {
+    let darkModeOn = false;
+    const darkMode = <h1>Dark Mode is on</h1>
+    const lightMode = <h1>Light Mode is on</h1>
     
+    function handleClick() {
+        darkModeOn = !darkModeOn
+        darkModeOn ? console.log("Dark Mode is on") : console.log("Light mode is on")
+    }
+
+    return (
+        <div>
+            {darkModeOn ? darkMode : lightMode}
+            <button onClick={handleClick}>
+                Click Me
+            </button>
+        </div>
+    )
 }
