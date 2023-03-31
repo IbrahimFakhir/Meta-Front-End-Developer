27b9c40e30fbb34e5de3c08cd0ef341b54f903a1
IbrahimFakhir <ibrahim.fakhir@outlook.de>
2023-03-31 15:03:02 +0200

    implemented uncontrolled input

--- firstapp/src/Form.js
+++ firstapp/src/Form.js
@@ -1,3 +1,21 @@
+import { useRef } from "react";
+
 export default function Form() {
     return <h1>yo</h1>
+}
+
+function UncontrolledInput() {
+    const inputRef = useRef(null);
+    
+    const handleSubmit = () => {
+        const inputValue = inputRef.current.value;
+        console.log(inputValue)
+    }
+
+    return (
+        <form onSubmit={ handleSubmit }>
+            <input ref={inputRef} type="text"></input>
+            <button type="submit">Submit</button>
+        </form>
+    )
 }
